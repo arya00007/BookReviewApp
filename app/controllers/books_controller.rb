36ec0b6677105cf3639a649fd5f01db138d41edd
arya00007 <animesharya.aa@gmail.com>
2022-08-17 15:09:38 +0530

    Controller Setup

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,35 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
-	def index	
-	end
+  def index
+    @books = Book.all.order('created_at DESC')
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :description, :author)
+  end
+
+  def find_book
+  	@book = Book.find(params[:id])
+  end
 end
